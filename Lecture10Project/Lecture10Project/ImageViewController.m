4092dd8781beed6043e527e3d737db1aa47237cf
larryhzhang <larryhzhang@tencent.com>
2020-09-03 21:25:00 +0800

    完成Lecture10大图片浏览器全部功能

--- Lecture10Project/Lecture10Project/ImageViewController.m
+++ Lecture10Project/Lecture10Project/ImageViewController.m
@@ -8,17 +8,27 @@
 
 #import "ImageViewController.h"
 
-@interface ImageViewController ()
+@interface ImageViewController () <UIScrollViewDelegate>
 @property(nonatomic, strong) UIImageView *imageView;
 @property(nonatomic,strong) UIImage *image;
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *spinner;
 @end
 
 @implementation ImageViewController
 
 - (void)setScrollView:(UIScrollView *)scrollView {
     _scrollView = scrollView;
+    _scrollView.minimumZoomScale = 0.2;
+    _scrollView.maximumZoomScale = 2.0;
+    _scrollView.delegate = self;
+    
+    //设置两次contSize的原因是我们不确保这两个方法哪个是先被调用的
     self.scrollView.contentSize = self.image ? self.image.size : CGSizeZero;
+}
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+    return self.imageView;
 }
 
 // getter of imageView
@@ -32,9 +42,32 @@
 // 错误做法：阻塞主线程
 - (void)setImageURL:(NSURL *)imageURL {
     _imageURL = imageURL;
-    self.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:imageURL]];
+//    self.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:imageURL]];
+    [self startDownloadingImage];
 }
 
+- (void)startDownloadingImage {
+    self.image = nil;
+    if (self.imageURL) {
+        // 转动组件
+        [self.spinner startAnimating];
+        
+        NSURLRequest *request = [NSURLRequest requestWithURL:self.imageURL];
+        NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration ephemeralSessionConfiguration];
+        NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
+        // callback需要放在其他线程
+        NSURLSessionDownloadTask *task = [session downloadTaskWithRequest:request
+                                                        completionHandler:^(NSURL *localfile, NSURLResponse *response, NSError *error) {
+            if (!error) {
+                if ([request.URL isEqual:self.imageURL]) {
+                    UIImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL:localfile]];
+                    dispatch_async(dispatch_get_main_queue(), ^{ self.image = image; });
+                }
+            }
+        }];
+        [task resume];
+    }
+}
 - (UIImage *)image {
     return self.imageView.image;
 }
@@ -43,6 +76,7 @@
     self.imageView.image = image;
     [self.imageView sizeToFit];
     self.scrollView.contentSize = self.image ? self.image.size : CGSizeZero;
+    [self.spinner stopAnimating];
 }
 
 - (void)viewDidLoad {
